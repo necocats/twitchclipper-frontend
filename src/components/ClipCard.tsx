f74ca8661caa292c1cbe49cbb237f9bd531413aa
39rep <okome3317@gmail.com>
2024-06-08 23:44:29 +0900

    delete_Home_Components_Routes

--- src/components/ClipCard.tsx
+++ src/components/ClipCard.tsx
@@ -12,8 +12,8 @@ const ClipCard: React.FC<ClipCardProps> = ({ title, broadcaster_name, thumbnail_
     <>
       <div className="card" onClick={onClick}>
         <img src={thumbnail_url} className="card-img-top" alt=""></img>
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
+        <div className="my-card-body">
+          <h5 className="my-card-title">{title}</h5>
           <p className="card-text">{broadcaster_name}</p>
         </div>
       </div>
